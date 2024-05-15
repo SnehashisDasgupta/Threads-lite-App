c6dc68d51823db311919e5003b4d9816019ae518
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-05-15 11:56:19 +0500

    Added getUserProfile route to see profiles of user without signing in

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,6 +2,21 @@ import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
 import generateTokenAndSetCookie from '../utils/generateTokenAndSetCookie.js';
 
+const getUserProfile = async (req, res) => {
+    const { username } = req.params;
+    try {
+        const user = await User.findOne({ username }).select("-password").select("-updatedAt");
+
+        if (!user) return res.status(400).json({ message: "User not found" });
+
+        res.status(200).json(user);
+        
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+        console.log("Error in getUserProfile: ", err.message);
+    }
+}
+
 const signupUser = async (req, res) => {
     try {
         const {name, username, email, password} = req.body;
@@ -156,4 +171,4 @@ const updateUser = async (req, res) => {
     }
 }
 
-export { signupUser, loginUser, logoutUser, followUnFollowUser, updateUser };
+export { signupUser, loginUser, logoutUser, followUnFollowUser, updateUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { followUnFollowUser, loginUser, logoutUser, signupUser, updateUser } from '../controllers/userControllers.js';
+import { followUnFollowUser, getUserProfile, loginUser, logoutUser, signupUser, updateUser } from '../controllers/userControllers.js';
 import protectRoute from '../middlewares/protectRoute.js';
 
 const router = express.Router();
 
+router.get('/profile/:username', getUserProfile);
 router.post('/signup', signupUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
