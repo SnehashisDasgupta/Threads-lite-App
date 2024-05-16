6adee8b026d9f82336e38a1d2afbcca0b85a746d
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-05-16 12:59:50 +0500

    Added likeUnlikePost route to like-dislike a post and store the userId in like array

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -69,4 +69,34 @@ const deletePost = async (req, res) => {
     }
 }
 
-export { createPost, getPost, deletePost };
+const likeUnlikePost = async (req, res) => {
+    try {
+        const {id: postId} = req.params;
+        const userId = req.user._id;
+
+        const post = await Post.findById(postId);
+        if(!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        //if userId present in like array, means user already liked the post
+        const userLikedPost = post.likes.includes(userId);
+        if(userLikedPost){
+            // Unlike post
+            await Post.updateOne({ _id:postId }, {$pull: { likes: userId } });
+            res.status(200).json({ message: "Post disliked successfully" });
+
+        }else{
+            // Like post
+            post.likes.push(userId);
+            await post.save();
+            res.status(200).json({ message: "Post liked successfully" });
+        }
+        
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+        console.log("Error in likeUnlikePost: ", err.message);
+    }
+}
+
+export { createPost, getPost, deletePost, likeUnlikePost };
--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -14,8 +14,10 @@ const postSchema = mongoose.Schema({
         type: String,
     },
     likes: {
-        type: Number,
-        default: 0,
+        // array of user ids
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'User',
+        default: [],
     },
     replies: [
         {
