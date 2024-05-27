49ee2766e1487dfc3605a5fcd31deef5f3ecec31
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-05-27 14:40:20 +0500

    Added like-unlike Post functionality

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -8,7 +8,6 @@ import useShowToast from '../hooks/useShowToast';
 import { formatDistanceToNow } from "date-fns";
 
 const Post = ({ post, postedBy }) => {
-    const [liked, setLiked] = useState(false);
     const [user, setUser] = useState(null);
     const showToast = useShowToast();
     const navigate = useNavigate();
@@ -143,21 +142,10 @@ const Post = ({ post, postedBy }) => {
 
                     {/* Action icons */}
                     <Flex gap={3} my={1}>
-                        <Actions liked={liked} setLiked={setLiked} />
+                        <Actions post={post} />
                     </Flex>
 
-                    {/* replies and likes */}
-                    <Flex gap={2} alignItems={"center"}>
-                        <Text color={"gray.light"} fontSize={"sm"}>
-                            {post.replies.length} replies
-                        </Text>
-
-                        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
-
-                        <Text color={"gray.light"} fontSize={"sm"}>
-                            {post.likes.length} likes
-                        </Text>
-                    </Flex>
+                    
                 </Flex>
             </Flex>
         </Link>
